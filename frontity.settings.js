81f7660899af18822770a4c1b5f1bc1734351ce1
henyudha <hyudha@gmail.com>
2022-04-21 14:23:14 +0700

    commit

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,57 +1,43 @@
 const settings = {
-  "name": "jejak",
-  "state": {
-    "frontity": {
-      "url": "https://test.frontity.org",
-      "title": "Test Frontity Blog",
-      "description": "WordPress installation for Frontity development"
-    }
+  name: "jejak",
+  state: {
+    frontity: {
+      url: "https://jejakonline.com",
+      title: "jejakonline",
+      description: "kepemimpinan, teknologi dan qalbu ihsani",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/mars-theme",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Nature",
-              "/category/nature/"
-            ],
-            [
-              "Travel",
-              "/category/travel/"
-            ],
-            [
-              "Japan",
-              "/tag/japan/"
-            ],
-            [
-              "About Us",
-              "/about-us/"
-            ]
+      name: "@frontity/mars-theme",
+      state: {
+        theme: {
+          menu: [
+            ["News", "/c/news/"],
+            ["Lifestyle", "/c/lifestyle/"],
+            ["Editorial", "/c/editorial/"],
+            ["Figur", "/c/figur/"],
+            ["Komunitas", "/c/komunitas/"],
           ],
-          "featured": {
-            "showOnList": false,
-            "showOnPost": false
-          }
-        }
-      }
+          featured: {
+            showOnList: true,
+            showOnPost: false,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "url": "https://test.frontity.org"
-        }
-      }
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          url: "https://jejakonline.com",
+          categoryBase: "c",
+        },
+      },
     },
     "@frontity/tiny-router",
-    "@frontity/html2react"
-  ]
+    "@frontity/html2react",
+  ],
 };
 
 export default settings;
